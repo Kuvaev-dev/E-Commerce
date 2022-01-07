755aec03eaf8f958e737aff5ccfa90b42fd9c014
Kuvaev-dev <likeaboss13371@gmail.com>
2022-01-07 21:02:40 +0200

    [frontend] Checkout Component Added

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CartService} from "../../services/cart.service";
+import {CartModelServer} from "../../models/cart.model";
+import {Router} from "@angular/router";
+import {OrderService} from "../../services/order.service";
+// @ts-ignore
+import {NgxSpinnerService} from "ngx-spinner";
+import {FormBuilder, NgForm, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'app-checkout',
+  selector: 'mg-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
+  // @ts-ignore
+  cartData: CartModelServer;
+  // @ts-ignore
+  cartTotal: Number;
+  // @ts-ignore
+  showSpinner: Boolean;
+  checkoutForm: any;
 
-  constructor() { }
+  constructor(private cartService: CartService,
+              private orderService: OrderService,
+              private router: Router,
+              private  spinner: NgxSpinnerService,
+              private fb: FormBuilder) {
 
-  ngOnInit(): void {
+    this.checkoutForm = this.fb.group({
+      firstname: ['', [Validators.required]],
+      lastname: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required]],
+    });
   }
 
+  ngOnInit() {
+    this.cartService.cartDataObs$.subscribe(data => this.cartData = data);
+    this.cartService.cartTotal$.subscribe(total => this.cartTotal = total);
+  }
+
+  onCheckout() {
+    this.spinner.show().then(p => {
+      this.cartService.CheckoutFromCart(1);
+    });
+
+    //console.log(this.checkoutForm.value);
+  }
 }
