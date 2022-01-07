eccd2ffb4b015652ff60090d062903c36c75f533
Kuvaev-dev <likeaboss13371@gmail.com>
2022-01-07 21:30:59 +0200

    [frontend] App Component Fixed

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,34 +1,48 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { CartComponent } from './components/cart/cart.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
-import { HomeComponent } from './components/home/home.component';
-import { ProductComponent } from './components/product/product.component';
-import { ThankyouComponent } from './components/thankyou/thankyou.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {HomeComponent} from './components/home/home.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {CartComponent} from './components/cart/cart.component';
+import {CheckoutComponent} from './components/checkout/checkout.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ProductComponent} from './components/product/product.component';
+import {ThankyouComponent} from './components/thankyou/thankyou.component';
+import {NgxSpinnerModule} from "ngx-spinner";
+import {BrowserAnimationsModule, NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ToastrModule} from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
     CartComponent,
     CheckoutComponent,
-    HomeComponent,
     ProductComponent,
     ThankyouComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    NoopAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgxSpinnerModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
