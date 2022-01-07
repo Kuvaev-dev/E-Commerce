c364cbed8cecd53ec63c60269cffe345b1448629
Kuvaev-dev <likeaboss13371@gmail.com>
2022-01-07 22:09:10 +0200

    [backend] App Routes Connected

--- backend/app.js
+++ backend/app.js
@@ -2,29 +2,60 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: "E-Commerce API",
+            description: "Backend Api",
+            contact: {
+                name: 'Amazing Developer'
+            },
+            servers: "http://localhost:3636"
+        }
+    },
+    apis: ["app.js", ".routes/*.js"]
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+
+/* CORS */
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST'],
+    allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
+app.use(logger('combined'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Import Routes
-const productRoute = require('./routes/products');
-const userRoute = require('./routes/users');
+const usersRouter = require('./routes/users');
+const productsRouter = require('./routes/products');
+const authRouter = require('./routes/auth');
+const orderRouter = require('./routes/order');
 
-// Use Routes
-app.use('/api/products', productRoute);
-app.use('/api/users', userRoute);
+// Define Routes
+/**
+ * @swagger
+ * /api/products:
+ *   get:
+ *    description: Get All Products
+ *
+ */
 
-app.use(cors({
-    origin: '*',
-    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
-    allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
-}));
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/users', usersRouter);
+app.use('/api/products', productsRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/orders', orderRouter);
 
 module.exports = app;
