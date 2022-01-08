0117f0b7dc1b1a22952df79897f9a2de932a3940
Kuvaev-dev <likeaboss13371@gmail.com>
2022-01-08 02:35:54 +0200

    [admin] Admin Panel Components Added

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllProductsComponent } from './components/all-products/all-products.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllProductsComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainContentComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
