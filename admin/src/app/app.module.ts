836f5ca3ad1fd25bd494e4b392ffff126dc5611c
Kuvaev-dev <likeaboss13371@gmail.com>
2022-01-08 02:43:32 +0200

    [admin] Header Template Component Added

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -1,28 +1,32 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AllProductsComponent } from './components/all-products/all-products.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { MainContentComponent } from './components/main-content/main-content.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { AllProductsComponent } from './components/all-products/all-products.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AllProductsComponent,
-    FooterComponent,
     HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
     MainContentComponent,
-    SidebarComponent
+    AllProductsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
